c17c4ab983495ca228b720ffa030767c8e53bc29
Fahmi Hadi <Kokolopo321@gmail.com>
2022-04-20 14:13:31 +0700

    add

--- config/config.go
+++ config/config.go
@@ -20,7 +20,7 @@ func InitConfiguration() Config {
 		SERVER_ADDRESS: GetOrDefault("SERVER_ADDRESS", "0.0.0.0:8080"),
 		DB_USERNAME:    GetOrDefault("DB_USERNAME", "root"),
 		DB_PASSWORD:    GetOrDefault("DB_PASSWORD", ""),
-		DB_NAME:        GetOrDefault("DB_NAME", "test"),
+		DB_NAME:        GetOrDefault("DB_NAME", "kuliah"),
 		DB_PORT:        GetOrDefault("DB_PORT", "3306"),
 		DB_HOST:        GetOrDefault("DB_HOST", "127.0.0.1"),
 		JWT_KEY:        GetOrDefault("JWT_KEY", "KudaLumping"),
